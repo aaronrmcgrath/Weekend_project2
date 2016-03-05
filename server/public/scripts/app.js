3639e0e2c445fd40609cba1204ce8b3316862e22
Mbob023 <armcgrathmedia@gmail.com>
2016-03-05 09:27:08 -0600

    next previous reconstruct nw

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,3 +1,6 @@
+
+
+//jQuery - waits until document is ready and loads/GET data from data.json
 $(document).ready(function(){
   $.ajax({
     type: "GET",
@@ -5,116 +8,122 @@ $(document).ready(function(){
     success: function(data){
       console.log(data);
       appendDom(data);
+    }
+  });
 
-      function onNext() {
-        index++;
-        // var person = data.kappa;
-        if (index > data.kappa.length - 1) {
-          index = 0;
-        }
-        $.ajax({
-          type: "GET",
-          url: "/data",
-          success: function(data){
-            console.log('onNext data: ', data);
-            appendDom(data);
-            console.log(index);
-            return index;
-          }})};
-
-          function onPrevious() {
-            index--;
-            // var person = data.kappa;
-            if (index < 0) {
-              index = data.kappa.length - 1;
-            }
-            $.ajax({
-              type: "GET",
-              url: "/data",
-              success: function(data){
-                console.log('onPrevious data: ', data);
-                appendDom(data);
-                console.log(index);
-                return index;
-              }})};
-
-              $('.carousel').on('click', '.next', onNext);
-              $('.carousel').on('click', '.previous', onPrevious);
-            }
-          });
-        });
-
-        var index = 0;
-        var $el = undefined;
-        // var person = undefined;
-
-        function appendDom(data) {
-          $('.carnival').append('<div class="carousel"></div>');
-
-          var $el = $('.carnival').children().last();
-          // person = data.kappa;
-
-          $el.append('<br />');
-          $el.append('<p>' + data.kappa[index].name + '</p>');
-          $el.append('<p>' + data.kappa[index].location + '</p>');
-          $el.append('<p>' + data.kappa[index].spirit_animal + '</p>');
-          $el.append('<p>' + data.kappa[index].shoutout + '</p>');
-
-          $el.append('<button type="button" class="previous btn">Previous</button>');
-          $el.append('<button type="button" class="next btn">Next</button>');
-
-          // function onNext() {
-          //   index++;
-          //   // var person = data.kappa;
-          //   if (index > data.kappa.length - 1) {
-          //     index = 0;
-          //   }
-          //   appendDom();
-          //   console.log(index);
-          //   // return index;
-          // }
-          //
-          // function onPrevious() {
-          //   index--;
-          //   // var person = data.kappa;
-          //   if (index < 0) {
-          //     index = data.kappa.length - 1;
-          //   }
-          //   appendDom();
-          //   console.log(index);
-          //   // return index;
-          // }
+  $('.carousel').on('click', '.next', onNext);
+  $('.carousel').on('click', '.previous', onPrevious);
+});
 
 
-          // $('.carousel').on('click', '.next', onNext);
-          // $('.carousel').on('click', '.previous', onPrevious);
+//GLOBAL VARIABLES
 
-        }
-
-
-        function onNext() {
-          index++;
-          // var person = data.kappa;
-          if (index > data.kappa.length - 1) {
-            index = 0;
-          }
-          // appendDom();
-          console.log(index);
-          return index;
-        }
-
-        function onPrevious() {
-          index--;
-          // var person = data.kappa;
-          if (index < 0) {
-            index = data.kappa.length - 1;
-          }
-          // appendDom();
-          console.log(index);
-          return index;
-        }
+var index = 0;
+var $el = undefined;
+// var person = undefined;
 
 
+
+//FUNCTIONS
+
+//Appends the Dom and creates the previous and next buttons
+function appendDom(data) {
+  $('.carnival').append('<div class="carousel"></div>');
+
+  var $el = $('.carnival').children().last();
+  // person = data.kappa;
+
+  $el.append('<br />');
+  $el.append('<p>' + data.kappa[index].name + '</p>');
+  $el.append('<p>' + data.kappa[index].location + '</p>');
+  $el.append('<p>' + data.kappa[index].spirit_animal + '</p>');
+  $el.append('<p>' + data.kappa[index].shoutout + '</p>');
+
+  $el.append('<button type="button" class="previous btn">Previous</button>');
+  $el.append('<button type="button" class="next btn">Next</button>');
+}
+
+
+//Controls the 'Next' button
+function onNext() {
+  index++;
+  // var person = data.kappa;
+  if (index > data.kappa.length - 1) {
+    index = 0;
+  }
+  $.ajax({
+    type: "GET",
+    url: "/data",
+    success: function(data){
+      console.log('onNext data: ', data);
+      $('.carnival').empty();
+      appendDom(data);
+      // console.log(index);
+      return index;
+    }
+  });
+  console.log(index);
+}
+
+
+//Controls the 'Previous' button
+function onPrevious() {
+  index--;
+  // var person = data.kappa;
+  if (index < 0) {
+    index = data.kappa.length - 1;
+  }
+  $.ajax({
+    type: "GET",
+    url: "/data",
+    success: function(data){
+      console.log('onPrevious data: ', data);
+      $('.carnival').empty();
+      appendDom(data);
+      // console.log(index);
+      return index;
+    }
+  });
+  console.log(index);
+}
+
+
+
+
+
+
+
+
+//END ------>>> trialed functions below v
+
+
+
+
+
+
+
+
+        // function onNext() {
+        //   index++;
+        //   // var person = data.kappa;
+        //   if (index > data.kappa.length - 1) {
+        //     index = 0;
+        //   }
+        //   // appendDom();
+        //   console.log(index);
+        //   return index;
+        // }
+        //
+        // function onPrevious() {
+        //   index--;
+        //   // var person = data.kappa;
+        //   if (index < 0) {
+        //     index = data.kappa.length - 1;
+        //   }
+        //   // appendDom();
+        //   console.log(index);
+        //   return index;
+        // }
 
 
 
